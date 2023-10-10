858ff3dd64e5d7a565d7f273ae1d1af155433619
tuxomup <112069849+tuxomup@users.noreply.github.com>
2023-10-10 12:25:04 +0200

    Create credentials.h

---
+++ credentials.h
@@ -0,0 +1,17 @@
+/****************************************************************************************************************************
+  credentials.h
+ ***************************************************************************************************************************************/
+
+#ifndef credentials_h
+#define credentials_h
+
+#define _ASYNC_MQTT_LOGLEVEL_               1
+
+#define ssid         "wifi_id" //wifi SSID
+#define password     "xxxx" //wifi password
+
+#define mqtt_server IPAddress(192, 168, 0, 1)
+#define MQTT_USER "user"   //mqtt username
+#define MQTT_PASSWORD "pass" //mqtt password
+
+#endif    //defines_h
